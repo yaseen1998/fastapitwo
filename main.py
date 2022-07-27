62e43e1054c2dc8c56aad84f22a465df26cdb388
yaseen1998 <yasseenkouthe@eng.hu.edu.jo>
2022-07-27 17:23:11 +0300

    minutes41

--- main.py
+++ main.py
@@ -11,3 +11,13 @@ def read_root():
 @app.get("/about/")
 def about():
     return {"about": "about"}
+
+
+@app.get('/items/unpublish/')
+def unpublish():
+    return {"unpublish": "unpublish"}
+
+@app.get("/items/{item_id}/")
+def item(item_id:int):
+    return {"item": item_id}
+
